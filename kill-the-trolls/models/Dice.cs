e52c0952c163626269da13cc4bb85eb0e1afdbee
Alex Johansen <alex@ennea.co.uk>
2020-08-21 16:41:15 +0100

    Add dice

---
+++ kill-the-trolls/models/Dice.cs
@@ -0,0 +1,13 @@
+namespace kill_the_trolls.models
+{
+    public enum Dice
+    {
+        D4 = 4,
+        D6 = 6,
+        D8 = 8,
+        D10 = 10,
+        D12 = 12,
+        D20 = 20,
+        D100 = 100
+    }
+}
